5a5bb55cf51e74fe26fd8185a425862a49d8c70c
MrMontag <MrMontagOpenDev@gmail.com>
2016-07-15 00:08:54 +0200

    csv2wav.sh recursive csv2wav processing.

---
+++ scripts/csv2wav.sh
@@ -0,0 +1,31 @@
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright (C)    2016  Alexander (MrMontag) Fust <MrMontagOpenDev@gmail.com>
+#
+# This script creates mp4 files for all h264 videos contained in a folder
+# and it's subdirectories. To run the processing the Open Source multimedia
+# framework GPAC <https://gpac.wp.mines-telecom.fr/> is needed. If it is not
+# yet installed please run the following commands (for Debian based systems).
+#
+#     sudo apt-get update
+#     sudo apt-get install gpac
+#
+#!/bin/bash
+    
+find -printf "%p\n" | grep -i -e ".*\.csv$" | while read -r fp
+do
+    echo "$fp"
+    ./csv2wav.py $fp ${fp/csv/wav}
+    echo ""
+done
